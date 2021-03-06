94e451ef895377307386f7c0fff185b92626333c
vaibhav <vaibhav.111padghan@gmail.com>
2021-03-06 23:53:57 +0530

    test 5 pranav

--- requests/endpoint.php
+++ requests/endpoint.php
@@ -1,20 +1,20 @@
 <?php
 $request_type = $_POST['request_type'];
-// $data = $_POST['data'];
-// $data = json_decode($data, true);
+$data = $_POST['data'];
+$data = json_decode($data, true);
 $response = $request_type;
-// $response = null;
+$response = null;
 
-// switch ($request_type) {
-//     case "signup":
-//         $response['status'] = true;
+switch ($request_type) {
+    case "signup":
+        $response['status'] = true;
 
-//         $response['username'] = $data['username'];
-//         $response['email'] = $data['email'];
-//         $response['phone'] = $data['phone'];
-//         $response['password'] = $data['password'];
-//         break;
-// }
+        $response['username'] = $data['username'];
+        $response['email'] = $data['email'];
+        $response['phone'] = $data['phone'];
+        $response['password'] = $data['password'];
+        break;
+}
 
-// $response = json_encode($response);
+$response = json_encode($response);
 echo $response;
