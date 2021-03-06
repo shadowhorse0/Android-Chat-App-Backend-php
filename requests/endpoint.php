e9e0395f00e89fc22ba226ac227790186311c2bf
vaibhav <vaibhav.111padghan@gmail.com>
2021-03-07 00:39:21 +0530

    invert comma error

--- requests/endpoint.php
+++ requests/endpoint.php
@@ -3,20 +3,20 @@ $request_type = $_POST['request_type'];
 $data = $_POST['data'];
 $data = json_decode($data, true);
 $response = $request_type;
-$response['hello']="helllo";
+$response['hello'] = "helllo";
 
 switch ($request_type) {
     case "signup":
         $response['status'] = true;
-        $username=$data['username'];
-        $email=$data['email'];
-        $phone=$data['phone'];
-        $password=$data['password'];
-
-        
+        $username = $data['username'];
+        $email = $data['email'];
+        $phone = $data['phone'];
+        $password = $data['password'];
 
 
-        $sql="INSERT INTO `users`(`username`, `email`, `password`, `phone`) VALUES ("$username","$email","$phone","$password")";
+
+
+        $sql = "INSERT INTO `users`(`username`, `email`, `password`, `phone`) VALUES ('$username','$email','$phone','$password')";
         $conn->query($sql);
         break;
 }
